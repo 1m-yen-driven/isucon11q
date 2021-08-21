bf9875030a086492853869e384e99f69c30b35e1
murata <murata@kmc.gr.jp>
2021-08-21 13:57:30 +0900

    post by isu uuid

--- go/main.go
+++ go/main.go
@@ -1219,16 +1219,18 @@ func InsertIsuConditionLoop() {
 // POST /api/condition/:jia_isu_uuid
 // ISUからのコンディションを受け取る
 func postIsuCondition(c echo.Context) error {
-	// TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
-	dropProbability := 0.9
-	if rand.Float64() <= dropProbability {
-		// c.Logger().Warnf("drop post isu condition request")
-		return c.NoContent(http.StatusAccepted)
-	}
-
 	jiaIsuUUID := c.Param("jia_isu_uuid")
 	if jiaIsuUUID == "" {
 		return c.String(http.StatusBadRequest, "missing: jia_isu_uuid")
+	}
+	// 0-9 a-f
+	if jiaIsuUUID[len(jiaIsuUUID)-1] >= '2' {
+		// TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
+		dropProbability := 0.9
+		if rand.Float64() <= dropProbability {
+			// c.Logger().Warnf("drop post isu condition request")
+			return c.NoContent(http.StatusAccepted)
+		}
 	}
 
 	req := []PostIsuConditionRequest{}
