3520383233c1de3574adf9bd96968ec77759a47f
nakario <nakario.jp@gmail.com>
2021-08-21 10:33:51 +0900

    fix dir name

--- deploy_golang.sh
+++ deploy_golang.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd golang
+cd go
 make cross-compile
 
 for app in "$@"; do
-  ssh -o ClearAllForwardings=yes -tt $app /home/isucon/logs/parse.sh skip
+  # ssh -o ClearAllForwardings=yes -tt $app /home/isucon/logs/parse.sh skip
   ssh -o ClearAllForwardings=yes $app sudo systemctl stop mysql isucondition.go.service nginx
   scp isucondition $app:/home/isucon/webapp/go/isucondition
   ssh -o ClearAllForwardings=yes $app sudo systemctl start mysql isucondition.go.service nginx
